b9557cf461ca4699ebeb2753a2a82489b94cb280
root <root@jbcrawford.us>
2012-09-09 22:56:56 -0700

    got wsgi.py to work after a mighty struggle worthy of greek myth, I got wsgi.py to work

--- nmticpc/wsgi.py
+++ nmticpc/wsgi.py
@@ -1,8 +1,14 @@
 import os
+import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nmticpc.settings")
 
+sys.path.append("/var/www-icpc/")
+sys.path.append("/var/www-icpc/nmticpc")
+
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
