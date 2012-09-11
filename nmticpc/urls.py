12cc5e0f43b25c29f5943ff6f3717bc91289a552
root <root@jbcrawford.us>
2012-09-10 22:59:58 -0700

    Enabled the auto-admin

--- nmticpc/urls.py
+++ nmticpc/urls.py
@@ -8,9 +8,6 @@ urlpatterns = patterns('',
     # Example:
     # (r'^nmticpc/', include('nmticpc.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
