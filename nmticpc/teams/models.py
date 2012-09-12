382756cb1b3d4895e2c51a316273b173c6c98a10
root <root@jbcrawford.us>
2012-09-11 21:38:51 -0700

    Added the initial models. Whoo.

--- nmticpc/teams/models.py
+++ nmticpc/teams/models.py
@@ -7,4 +7,16 @@ from userena.models import UserenaBaseProfile
 
 class TeamProfile(UserenaBaseProfile):
     user = models.OneToOneField(User, unique=True, verbose_name=('user'), related_name='team_profile')
-    
+    displayName = models.CharField(max_length=30)
+
+class Problem(models.Model):
+    number = models.IntegerField()
+    value  = models.IntegerField()
+
+class Submission(models.Model):
+    team = models.ForeignKey(TeamProfile)
+    author = models.ForeignKey(Problem)
+    reviewed = models.BooleanField()
+    valid = models.BooleanField()
+    comment = models.TextField()
+    content = models.FileField(upload_to="/submissions/%Y-%m-%d-%H%M%S")
